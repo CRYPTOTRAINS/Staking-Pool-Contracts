72b5e07fcbc244010996bf217c289c63e5a3fb1b
oxenprogrammer <emmy1000okello@gmail.com>
2022-01-22 19:18:37 +0300

    feat(stake): add button

---
+++ client/src/modules/common/Button/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css';
+import button from '../../../assets/images/botondebanco.png';
+
+const Button = () => {
+  return (
+    <main className="button">
+      <img src={button} alt="boton de banco" />
+    </main>
+  );
+};
+
+export default Button;
