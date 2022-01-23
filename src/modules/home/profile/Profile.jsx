3f13bf3c8ba48bf3da5e70ba58cf65619460f52d
george-shammar <george.gbenle@outlook.com>
2022-01-23 17:36:14 +0000

    Connect contracts

--- src/modules/home/profile/Profile.jsx
+++ src/modules/home/profile/Profile.jsx
@@ -2,6 +2,8 @@
 import './Profile.css';
 import { useEffect, useState } from 'react';
 import { connectWallet, getCurrentWalletConnected } from "../../../utils/wallet";
+import StakingAddress from "../../../contracts/contract-address.json";
+import StakingArtifact from "../../../contracts/StakeToken.json";
 
 const Profile = () => {
   const [coins, setCoins] = useState(0);
