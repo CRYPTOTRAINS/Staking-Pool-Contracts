1340540d04d148c625367a1ecf772c2553cda597
oxenprogrammer <emmy1000okello@gmail.com>
2022-01-23 11:01:41 +0300

    feat(stake): update profile

--- client/src/modules/home/profile/Profile.jsx
+++ client/src/modules/home/profile/Profile.jsx
@@ -1,13 +1,18 @@
+/* eslint-disable no-unused-vars */
 import './Profile.css';
+import { useState } from 'react';
 
 const Profile = () => {
+  const [coins, setCoins] = useState(0);
+  const [user, setUser] = useState('123..6777');
+  
   return (
     <main className="profile">
       <div className="coin-img">
-        <div className="coins">0</div>
+        <div className="coins">{coins}</div>
       </div>
       <div className="account-img">
-        <div className="accounts">Account</div>
+        <div className="accounts">{user}</div>
       </div>
     </main>
   );
