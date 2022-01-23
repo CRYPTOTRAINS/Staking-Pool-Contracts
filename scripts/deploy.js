9b5367de7346dfc6035326b6ff1e067f0ad960e6
george-shammar <george.gbenle@outlook.com>
2022-01-23 17:25:23 +0000

    Create deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,35 @@
-const { ethers } = require("ethers");
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const { ethers } = require("hardhat");
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
   
     console.log("Deploying contracts with the account:", deployer.address);
   
     console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-    // const MageToken = await ethers.getContractFactory("MageToken");
-    // const mageToken = await MageToken.deploy();
 
     const ENMT = await ethers.getContractFactory("ENMT");
-    const enmt =await ENMT.deploy("CTRAIN", "CTR", 18, 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266, 30000000);
+    const enmt =await ENMT.deploy("CTRAIN", "CTR", 18, "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", 30000000);
 
     const StakeToken = await ethers.getContractFactory("StakeToken");
     const staketoken = await StakeToken.deploy(enmt.address, enmt.address);
 
-  
-    // console.log("MageToken address:", mageToken.address);
     console.log("ENMT address:", enmt.address);
     console.log("StakeToken address:", staketoken.address);
 
+    let config = `
+    export const azaniamarketaddress = "${enmt.address}"
+    export const azaniaaddress = "${staketoken.address}"
+      `
+    let data = JSON.stringify(config)
+    fs.writeFileSync('config.js', JSON.parse(data))
+
     //  To save the contract's artifacts and address in the frontend directory
     saveFrontendFiles(enmt);
-    saveFrontendFiles(staketoken);
+    saveFrontend(staketoken);
   }
+
 
   function saveFrontendFiles(enmt) {
     const fs = require("fs");
@@ -36,24 +41,41 @@ async function main() {
   
     fs.writeFileSync(
       contractsDir + "/contract-address.json",
-      JSON.stringify({ ENMT: enmt.address }, undefined, 2),
-      JSON.stringify({ StakeToken: staketoken.address }, undefined, 2)
+      JSON.stringify({ ENMT: enmt.address }, undefined, 2)
     );
   
-    const ENMTTokenArtifact = artifacts.readArtifactSync("ENMT");
-    const StakeTokenArtifact = artifacts.readArtifactSync("StakeToken");
+    const ENMTArtifact = artifacts.readArtifactSync("ENMT");
   
     fs.writeFileSync(
       contractsDir + "/ENMT.json",
-      JSON.stringify(ENMTTokenArtifact, null, 2),
+      JSON.stringify(ENMTArtifact, null, 2)
+    );
+  }
+
+  function saveFrontend(staketoken) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../src/contracts";
+  
+    if (!fs.existsSync(contractsDir)) {
+      fs.mkdirSync(contractsDir);
+    }
+  
+    fs.writeFileSync(
+      contractsDir + "/contract-address.json",
+      JSON.stringify({ StakeToken: staketoken.address }, undefined, 2)
+    );
+  
+    const StakeTokenArtifact = artifacts.readArtifactSync("StakeToken");
+  
+    fs.writeFileSync(
+      contractsDir + "/StakeToken.json",
       JSON.stringify(StakeTokenArtifact, null, 2)
     );
   }
-  
+
   main()
     .then(() => process.exit(0))
     .catch((error) => {
       console.error(error);
       process.exit(1);
     });
-  
