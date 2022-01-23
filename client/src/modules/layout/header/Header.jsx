8821e74ebd4066288b2106631e3079d3803d7bfe
oxenprogrammer <emmy1000okello@gmail.com>
2022-01-23 14:38:14 +0300

    feat(stake): finish mobile version

--- client/src/modules/layout/header/Header.jsx
+++ client/src/modules/layout/header/Header.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import './Header.css';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/images/logo.png';
@@ -5,14 +7,35 @@ import Profile from '../../home/profile/Profile';
 
 const Header = () => {
   return (
-    <nav className="header">
-      <img className='logo' src={logo} alt='logo' />
-      <header className='link-container'>
-        <Link className='link-item' to="/train">Trains</Link> 
-        <Link className='link-item' to="/">Staking</Link>
-      </header>
-      <Profile />
-    </nav>
+    <>
+      <nav className="header">
+        <img className="logo" src={logo} alt="logo" />
+        <header className="link-container">
+          <Link className="link-item" to="/train">
+            Trains
+          </Link>
+          <Link className="link-item" to="/">
+            Staking
+          </Link>
+        </header>
+        <Profile />
+      </nav>
+      <nav id="hamnav">
+        <img className="logo" src={logo} alt="logo" />
+        <label htmlFor="hamburger">&#9776;</label>
+        <input type="checkbox" id="hamburger" />
+
+        <div id="hamitems">
+          <Link className="link-item" to="/train">
+            Trains
+          </Link>
+          <Link className="link-item" to="/">
+            Staking
+          </Link>
+          <Profile />
+        </div>
+      </nav>
+    </>
   );
 };
 
