60111d840f4397062fe6961d10244fef090517f5
oxenprogrammer <emmy1000okello@gmail.com>
2022-01-26 00:13:29 +0300

    feat(presale): add presale

--- src/modules/home/presale/Presale.jsx
+++ src/modules/home/presale/Presale.jsx
@@ -1,5 +1,7 @@
 import './Presale.css';
 import backgroundImage from '../../../assets/images/fondoNFTPresale.png';
+import approve from '../../../assets/images/approve.png';
+import loading from '../../../assets/images/loading.png';
 
 export const Presale = () => {
   return (
@@ -24,8 +26,20 @@ export const Presale = () => {
           <p className="reminder">SECONDS</p>
         </article>
       </section>
-      <section className='presale-main'>
-        <article className="presale-img"></article>
+      <section className="presale-main">
+        <article className="presale-img">
+          <div className="presale-main-items">
+            <p className="quantity">QUANTITY: 10000</p>
+            <button className="approve">
+              <img src={approve} alt="approve button" />
+            </button>
+            <p className="congrats">congrats your wallet is whitelisted</p>
+            <div className="sales-progress">
+              <p className="sales-progress-text">SALE PROGRESS</p>
+              <img src={loading} alt="sales progress loading" />
+            </div>
+          </div>
+        </article>
       </section>
     </main>
   );
