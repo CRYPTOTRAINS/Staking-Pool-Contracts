4879270d55490e4b4890d8ce84bbf1337b454850
oxenprogrammer <emmy1000okello@gmail.com>
2022-01-25 16:39:15 +0300

    feat(train): add background

--- src/modules/home/train/TrainList.jsx
+++ src/modules/home/train/TrainList.jsx
@@ -1,10 +1,30 @@
+/* eslint-disable  jsx-a11y/no-noninteractive-element-interactions
+ */
+/* eslint-disable no-unused-vars */
 import './TrainList.css';
-import TrainItem from './TrainItem';
+import './TrainItem.css';
+import BackArrow from '../../common/BackArrow/BackArrow';
 
 export const TrainList = () => {
+
   return (
-    <div className="train-list">
-      <TrainItem />
-    </div>
+    <>
+      <h2 className="title">Your Trains</h2>
+      <div className="train-list">
+        {/*=============POOL ONE ======================*/}
+        <main className="train"> 
+           
+        </main>
+        <main className="train"> 
+           
+        </main>
+        <main className="train"> 
+           
+        </main>
+        <main className="train"> 
+           
+        </main>
+      </div>
+    </>
   );
 };
