6c759e26d583728e232ea48905cee72b16b65827
oxenprogrammer <emmy1000okello@gmail.com>
2022-01-26 18:32:03 +0300

    feat(train): update icon names

--- src/modules/home/train/TrainList.jsx
+++ src/modules/home/train/TrainList.jsx
@@ -4,7 +4,10 @@
 import './TrainList.css';
 import './TrainItem.css';
 import BackArrow from '../../common/BackArrow/BackArrow';
-
+import common from '../../../assets/images/common_train.png'
+import rare from '../../../assets/images/rare_train.png'
+import epic from '../../../assets/images/epic_train.png'
+import legendary from '../../../assets/images/legendary_train.png'
 export const TrainList = () => {
 
   return (
@@ -13,19 +16,19 @@ export const TrainList = () => {
       <div className="train-list">
         {/*=============TRAIN ONE ======================*/}
         <main className="train"> 
-           
+          <img src={common} alt='common train' />
         </main>
         {/*=============TRAIN TWO ======================*/}
         <main className="train"> 
-           
+          <img src={rare} alt='rare train' />
         </main>
         {/*=============TRAIN THREE ======================*/}
         <main className="train"> 
-           
+          <img src={epic} alt='epic train' />
         </main>
         {/*=============TRAIN FOUR ======================*/}
         <main className="train"> 
-           
+          <img src={legendary} alt='legendary train' />
         </main>
       </div>
     </>
