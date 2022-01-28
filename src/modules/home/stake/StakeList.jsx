c159ddd1649264927e5700b0536e3317b9c920f3
George <george@velascommerce.com>
2022-01-28 12:00:31 +0000

    Update code

--- src/modules/home/stake/StakeList.jsx
+++ src/modules/home/stake/StakeList.jsx
@@ -32,7 +32,6 @@ export const StakeList = () => {
 // ================ pool one ======================
   async function stakeOne() {
     const {amount} = formInput;
-    console.log(amount);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(StakingAddress.StakeToken, StakingArtifact.abi, signer);
