4a62be4176780709cec871e2b8d384d90f230b7c
george-shammar <george.gbenle@outlook.com>
2022-01-31 21:02:35 +0000

    Deploy contract

--- src/modules/home/presale/Presale.jsx
+++ src/modules/home/presale/Presale.jsx
@@ -1,13 +1,63 @@
+/* eslint-disable  jsx-a11y/no-noninteractive-element-interactions
+ */
 import './Presale.css';
 import backgroundImage from '../../../assets/images/fondoNFTPresale.png';
 import approve from '../../../assets/images/approve.png';
 import loading from '../../../assets/images/loading.png';
+import CtrainAddress from "../../../contracts/ctrain-address.json";
+import CtrainArtifact from "../../../contracts/Ctrain.json";
+import ENMTAddress from "../../../contracts/ENMT-address.json";
+import ENMTArtifact from "../../../contracts/ENMT.json";
+import { ethers } from "ethers";
+import { useState } from 'react';
+
+const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
 
 export const Presale = () => {
+  const [status, setStatus] = useState("");
+  const [formInput, updateFormInput] = useState({no:0});
+
+  async function createToken() {
+    const no = formInput;
+   
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(CtrainAddress.Ctrain, CtrainArtifact.abi, signer);
+    const token = new ethers.Contract(ENMTAddress.ENMT, ENMTArtifact.abi, signer);
+    
+    const price = await contract.getMintingPrice(no);
+    const amt = JSON.stringify(price);
+   
+
+    try {
+      const tx = await token.approve(CtrainAddress.Ctrain, amt);
+      await tx.wait();
+
+      const transaction = await contract.createToken(no);
+      const receipt = await transaction.wait();
+      if (receipt.status === 0) {
+        console.log("failed transaction");
+        setStatus("Transaction failed");
+        throw new Error("Transaction failed");
+      } else {
+        setStatus("Transaction successful");
+      }
+
+    } catch(error) {
+      if (error.code === ERROR_CODE_TX_REJECTED_BY_USER) {
+        return;
+      }
+      console.error(error.message);
+      setStatus(console.error(error.message));
+    }
+  }
+
   return (
     <main>
       <img className="bg" src={backgroundImage} alt="background" />
       <h1 className="presale-header">NFT PRESALE</h1>
+      <p>{status}</p>
       <section className="time-cards">
         <article className="card days">
           <p className="number">00</p>
@@ -29,9 +79,11 @@ export const Presale = () => {
       <section className="presale-main">
         <article className="presale-img">
           <div className="presale-main-items">
-            <p className="quantity">QUANTITY: 10000</p>
+            <p className="quantity">QUANTITY: </p>
+            <input placeholder="E.g 2" required className="no"
+                  onChange={e => updateFormInput({...formInput, amount: e.target.value})}  />
             <button className="approve">
-              <img src={approve} alt="approve button" />
+              <img src={approve} onClick={createToken} onKeyDown={createToken} alt="approve button" />
             </button>
             <p className="congrats">congrats your wallet is whitelisted</p>
             <div className="sales-progress">
