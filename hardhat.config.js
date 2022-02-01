d4504003104d9507f63517c5776b0bf227e2368c
george-shammar <george.gbenle@outlook.com>
2022-02-01 22:06:17 +0000

    Update presale frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,7 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: {mnemonic: "atom target fun train arrest post swim glass math split crop wheel"}
+      accounts: {mnemonic: ""}
     },
     // mainnet: {
     //   url: "https://bsc-dataseed.binance.org/",
