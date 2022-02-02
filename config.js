8df6763adc8ee5426cf74156d205ed06d0dfefbb
george-shammar <george.gbenle@outlook.com>
2022-02-02 20:44:57 +0000

    Update frontend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-    export const enmtaddress = "0x215EBfD999BAB7a714617b6F3D7931ebF11c6f70"
-    export const staketokenaddress = "0xAA4F0dBaE27CCd8eab51527d754E3d458C1217d6"
-    export const ctrainaddress = "0x92374ADF336dDA82794163DF28CC6A5B2De0c7E5"
+    export const enmtaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    export const staketokenaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+    export const ctrainaddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
       
