cc03d349b70de7ba20416e2d74353fed377ae2e7
Emanuel Okello <emanuelokello@Emanuels-MacBook-Air.local>
2022-02-07 11:58:19 +0300

    feat(train): update Buy Ticket Modal

--- src/modules/common/Modal/Modal.jsx
+++ src/modules/common/Modal/Modal.jsx
@@ -4,8 +4,8 @@ import styles from './Modal.module.css';
 export default function Modal(props) {
   return (
     <div className={classNames("modal d-block", classNames)}>
-      <div className={classNames("modal-dialog", classNames)}>
-        <div className={classNames("modal-content", styles.content, classNames)}>
+      <div className={classNames("modal-dialog", styles.dialog)}>
+        <div className={classNames("modal-content", styles.content, props.className)}>
           { props.children }
         </div>
       </div>
--- src/modules/home/TrainModals/BuyTicketModal.jsx
+++ src/modules/home/TrainModals/BuyTicketModal.jsx
@@ -5,10 +5,18 @@ import BuyCTrainButton from '../../common/BuyCTrainButton/BuyCTrainButton';
 
 export default function BuyTicketModal(props) {
   return (
-    <Modal>
+    <Modal className="modal">
       <button className="close" onClick={props.close}></button>
       <img className="buy-ticket" src={buy_ticket} alt="buy ticket" />
       <div className="price">Price: 2 $CTRAIN per ticket</div>
+      <select name="select-train" id="select-train" className="train-select">
+        <option selected value="volvo">
+          15 Tickets for 28 $CTRAIN
+        </option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
       <div className="buy-button">
         <BuyCTrainButton handleOnClick={props.close} cTrainValue={'Buy Tickets'} />
       </div>
