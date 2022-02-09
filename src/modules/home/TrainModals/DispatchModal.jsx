e7a0b01b9a4ea0186bae6e9b4522bc5f213c005c
Emanuel Okello <emanuelokello@Emanuels-MacBook-Air.local>
2022-02-09 11:39:08 +0300

    feat(train): add dispatching modal

--- src/modules/home/TrainModals/DispatchModal.jsx
+++ src/modules/home/TrainModals/DispatchModal.jsx
@@ -3,22 +3,31 @@ import classNames from 'classnames';
 import BuyCTrainButton from '../../common/BuyCTrainButton/BuyCTrainButton';
 
 import styles from './DispatchModal.module.css';
+import ModalRoot from '../../common/Modal/ModalRoot';
+import ModalService from '../../../services/ModalService';
+import DispatchingModal from './DispatchingModal';
 
 export default function DispatchModal(props) {
+  const addDispatchingModal = () => {
+    ModalService.open(DispatchingModal);
+  };
   return (
-    <Modal>
-      <form className={classNames('dispatchModal')}>
-        <div>
-          <div className={styles.captcha}>25p2m</div>
-        </div>
-        <div>
-          <input className={styles.captchaInput} type="text" placeholder='captcha' />
-        </div>
-        <div className={styles.dispatchContainer}>
-          <BuyCTrainButton handleOnClick={props.close} cTrainValue={'Dispatch'} />
-          <BuyCTrainButton  handleOnClick={props.close}  cTrainValue={'Cancel'} />
-        </div>
-      </form>
-    </Modal>
+    <>
+      <ModalRoot />
+      <Modal>
+        <form className={classNames('dispatchModal')}>
+          <div>
+            <div className={styles.captcha}>25p2m</div>
+          </div>
+          <div>
+            <input className={styles.captchaInput} type="text" placeholder="captcha" />
+          </div>
+          <div className={styles.dispatchContainer}>
+            <BuyCTrainButton handleOnClick={addDispatchingModal} cTrainValue={'Dispatch'} />
+            <BuyCTrainButton handleOnClick={props.close} cTrainValue={'Cancel'} />
+          </div>
+        </form>
+      </Modal>
+    </>
   );
 }
