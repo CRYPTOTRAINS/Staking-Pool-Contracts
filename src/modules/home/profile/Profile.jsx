7a96d12e82ffdc7640022fff9c4668948206511c
Emanuel Okello <emanuelokello@Emanuels-MacBook-Air.local>
2022-02-10 21:15:57 +0300

    feat(train): add profile modal

--- src/modules/home/profile/Profile.jsx
+++ src/modules/home/profile/Profile.jsx
@@ -9,9 +9,19 @@ import ENMTArtifact from '../../../contracts/ENMT.json';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
+import ProfileModal from '../TrainModals/ProfileModal';
+import ModalService from '../../../services/ModalService';
+import ModalRoot from '../../common/Modal/ModalRoot';
+
 const Profile = () => {
   const [walletAddress, setWallet] = useState('');
   const [balance, setBalance] = useState('');
+  const [rootStatus, setRootStatus] = useState(false);
+
+  const addProfileModal = () => {
+    setRootStatus(!rootStatus);
+    ModalService.open(ProfileModal);
+  };
 
   useEffect(() => {
     (async () => {
@@ -71,39 +81,44 @@ const Profile = () => {
   }
 
   return (
-    <main className="profile">
-      <div className="coin-img">
-        <div className="coins">{balance}</div>
-      </div>
-      <div className="user">
-        <div className="account-img">
-          {walletAddress.length > 0 ? (
-            <div className="accounts">
-              {String(walletAddress).substring(0, 6) + '...' + String(walletAddress).substring(38)}
-            </div>
-          ) : (
-            <div
-              className="accounts"
-              role="button"
-              tabIndex={0}
-              onClick={connectWalletPressed}
-              onKeyDown={connectWalletPressed}
-            >
-              Connect Wallet
-            </div>
-          )}
+    <>
+      {rootStatus ? <ModalRoot /> : ''}
+      <main className="profile">
+        <div className="coin-img">
+          <div className="coins">{balance}</div>
         </div>
-        <OverlayTrigger
-          delay={{ hide: 450, show: 300 }}
-          overlay={(props) => <Tooltip {...props}>Click to View Profile</Tooltip>}
-          placement="bottom"
-        >
-          <div className="user-profile">
-            <div className="invisible">user</div>
+        <div className="user">
+          <div className="account-img">
+            {walletAddress.length > 0 ? (
+              <div className="accounts">
+                {String(walletAddress).substring(0, 6) +
+                  '...' +
+                  String(walletAddress).substring(38)}
+              </div>
+            ) : (
+              <div
+                className="accounts"
+                role="button"
+                tabIndex={0}
+                onClick={connectWalletPressed}
+                onKeyDown={connectWalletPressed}
+              >
+                Connect Wallet
+              </div>
+            )}
           </div>
-        </OverlayTrigger>
-      </div>
-    </main>
+          <OverlayTrigger
+            delay={{ hide: 450, show: 300 }}
+            overlay={(props) => <Tooltip {...props}>Click to View Profile</Tooltip>}
+            placement="bottom"
+          >
+            <button className="user-profile" onClick={addProfileModal}>
+              <div className="invisible">user</div>
+            </button>
+          </OverlayTrigger>
+        </div>
+      </main>
+    </>
   );
 };
 
