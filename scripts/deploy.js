7c49300a7fa9e2ee175f5054a9c84a5498edf600
george-shammar <george.gbenle@outlook.com>
2022-02-20 23:02:26 +0000

    Update contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -20,7 +20,7 @@ async function main() {
     const Ctrain = await ethers.getContractFactory("Ctrain");
     const ctrain = await Ctrain.deploy(enmt.address,"0x8Eea93cb015d764CbbFe6c21170fe3c0E81eC373");
 
-    const Ctrain = await ethers.getContractFactory("Ctrain");
+    const MarketPlace = await ethers.getContractFactory("MarketPlace");
     const ctrain = await Ctrain.deploy(enmt.address,"0x8Eea93cb015d764CbbFe6c21170fe3c0E81eC373")
 
     console.log("ENMT address:", enmt.address);
