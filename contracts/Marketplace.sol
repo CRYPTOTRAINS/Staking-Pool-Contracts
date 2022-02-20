1bc533888eb6f2453bea56b593261e41ff50b75d
george-shammar <george.gbenle@outlook.com>
2022-02-20 23:50:57 +0000

    Deploy contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract CtrainMarket is ReentrancyGuard {
+contract MarketPlace is ReentrancyGuard {
   using Counters for Counters.Counter;
   Counters.Counter private _itemIds;
   Counters.Counter private _itemsSold;
