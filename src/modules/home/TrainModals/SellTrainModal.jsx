0cdf99bf51bf827d42ae7bf2d08e05675e2228e4
george-shammar <george.gbenle@outlook.com>
2022-02-22 11:54:06 +0000

    Implement modals

--- src/modules/home/TrainModals/SellTrainModal.jsx
+++ src/modules/home/TrainModals/SellTrainModal.jsx
@@ -3,7 +3,7 @@
 
 import './FuelModal.css';
 import Modal from '../../common/Modal/Modal';
-import buy_ticket from '../../../assets/images/buy.png';
+import buy_train from '../../../assets/images/box.png';
 import BuyCTrainButton from '../../common/BuyCTrainButton/BuyCTrainButton';
 import { ethers } from "ethers";
 import CtrainAddress from "../../../contracts/ctrain-address.json";
@@ -57,11 +57,11 @@ export default function BuyTicketModal(props) {
   return (
     <Modal className="modal">
       <button className="close" onClick={props.close}></button>
-      {/* <img className="buy-ticket" src={buy_ticket} alt="buy ticket" /> */}
+      <img className="buy-train" src={buy_train} alt="buy train" />
       <p className="quantity">Price:  <input placeholder="600" required className="nom"
                   onChange={e => updateFormNumber({...formNumber, no: e.target.value})}  /></p>
       <div className="buy-button">
-        <BuyCTrainButton handleOnClick={props.close, sell} cTrainValue={'Buy Train'}   />
+        <BuyCTrainButton handleOnClick={props.close, sell} cTrainValue={'Sell Train'}   />
       </div>
     </Modal>
   );
